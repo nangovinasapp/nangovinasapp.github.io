6792cd912db15427267427cf424ff6681994cee7
Na Ngo <>
2025-06-06 11:58:21 +0700

    feat: update new web demo

--- demotration/sliding_puzzle/flutter_bootstrap.js
+++ demotration/sliding_puzzle/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"109150893958777c8f2215f6cfd3e89e984e8d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1852469528"
+    serviceWorkerVersion: "2449533018"
   }
 });
